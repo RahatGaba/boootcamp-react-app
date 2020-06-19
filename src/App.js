bec51333516ea257dcc85387714b62ca8c5e6be9
rahatgaba <rahatgaba89@gmail.com>
2020-06-19 06:03:42 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
-Hello World from me rahat gaba hojaaaa plzzzzzz
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Hello World
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
     </div>
   );
 }
